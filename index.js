0e59c4e706e9bf929e64735a1b3be5108be8d5df
Alex Fabiano <afabiano15@gmail.com>
2017-05-22 11:51:19 -0400

    separando os middlewares

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 import express from "express";
 import consign from "consign";
-const PORT = 3000;
 
 const app = express();
 
-app.set("json spaces", 4); //formata e tabula de forma amigável o json
-
 consign()
 	.include("models")
+	.then("libs/middlewares.js")
 	.then("routes")
+	.then("libs/boot.js")
 	.into(app);
-
-app.listen(PORT, () => console.log(`NTask API - porta ${PORT}`));
---
+++ libs/middlewares.js
@@ -0,0 +1,4 @@
+module.exports = app => {
+	app.set("port", 3000);
+	app.set("json spaces", 4); //formata e tabula de forma amigável o json
+}
