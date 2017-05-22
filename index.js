a8f5b0a35d4ed3d80c6fc94c0005fafbf1447939
Alex Fabiano <afabiano15@gmail.com>
2017-05-22 11:24:57 -0400

    separando as rotas

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import express from "express";
+import consign from "consign";
 const PORT = 3000;
-const app = express();
-app.get("/", (req, res) => res.json({status: "NTask API"}));
 
-app.get("/tasks", (req, res) => {
-	res.json({
-		tasks: [
-			{title: "Fazer compras"},
-			{title: "Consertar o pc"},
-		]
-	})
-})
+const app = express();
+
+app.set("json spaces", 4); //formata e tabula de forma amigável o json
+
+consign()
+	.include("routes")
+	.into(app);
 
 app.listen(PORT, () => console.log(`NTask API - porta ${PORT}`));
