8ecb92bad4c11a3f7c4c77bd10c8f319bb5fe8bc
Alex Fabiano <afabiano15@gmail.com>
2017-05-22 11:02:43 -0400

    adicionando novo endpoint /tasks

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ import express from "express";
 const PORT = 3000;
 const app = express();
 app.get("/", (req, res) => res.json({status: "NTask API"}));
+
+app.get("/tasks", (req, res) => {
+	res.json({
+		tasks: [
+			{title: "Fazer compras"},
+			{title: "Consertar o pc"},
+		]
+	})
+})
+
 app.listen(PORT, () => console.log(`NTask API - porta ${PORT}`));
