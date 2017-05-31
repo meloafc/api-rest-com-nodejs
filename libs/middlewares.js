1e5ccb3215cee7e56d551da098149bfc7a11062a
Alex Fabiano <afabiano15@gmail.com>
2017-05-31 09:50:25 -0400

    refatorando

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,4 +1,11 @@
+import bodyParser from "body-parser";
+
 module.exports = app => {
 	app.set("port", 3000);
 	app.set("json spaces", 4); //formata e tabula de forma amigável o json
+	app.use(bodyParser.json());
+	app.use((req, res, next) => {
+		delete req.body.id;
+		next();
+	})
 }
