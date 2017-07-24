d84a260f2b80b1469e7e9d72854ec52010911d95
Alex Fabiano <afabiano15@gmail.com>
2017-07-24 09:16:54 -0400

    instalando PASSPORT e JWT na api

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -4,6 +4,7 @@ module.exports = app => {
 	app.set("port", 3000);
 	app.set("json spaces", 4); //formata e tabula de forma amigável o json
 	app.use(bodyParser.json());
+	app.use(app.auth.initialize());
 	app.use((req, res, next) => {
 		delete req.body.id;
 		next();
